030bbade9f14760073745b9765d1acbd85c2b025
gifino1 <gifino@trisquare.com.my>
2024-07-12 19:11:43 +0700

    first inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+}
